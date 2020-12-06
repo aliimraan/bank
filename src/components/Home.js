440224e63831a24c91494ad84c0a6546bdfb27aa
unknown <aimraan204@gmail.com>
2020-12-07 02:39:34 +0530

    next

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from '../containers/Form'
+import Header from '../containers/Header'
+import Slider from '../containers/Slider'
+
+export default function Home() {
+    return (
+        <div>
+         <Form/>
+        </div>
+    )
+}
